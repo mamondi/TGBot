fe2500121f0b2a9ee86b99abd631c517fa3cdacb
Prytula Dmytro Vitaliyovych <dimamon02@gmail.com>
2024-08-26 13:51:13 +0300

    done

---
+++ TGBot/Host.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace TGBot
+{
+    public class Host
+    {
+        public Action<ITelegramBotClient, Update>? OnMessage;
+        private TelegramBotClient _bot;
+
+        public Host(string bottoken)
+        {
+            _bot = new TelegramBotClient(bottoken);
+        }
+        
+        public void BotStart()
+        {
+            _bot.StartReceiving(UpdateHandler, ErrorHandler);
+            Console.WriteLine("ON");
+        }
+
+        private async Task ErrorHandler(ITelegramBotClient client, Exception exception, CancellationToken token)
+        {
+            Console.WriteLine("Error " +  exception.Message);
+            await Task.CompletedTask;
+
+        }
+
+        private async Task UpdateHandler(ITelegramBotClient client, Update update, CancellationToken token)
+        {
+            Console.WriteLine($"Message: {update.Message?.Text ?? "ne text"}");
+            OnMessage?.Invoke(client, update);
+            await Task.CompletedTask;
+        }   
+    }
+}
--- TGBot/Program.cs
+++ TGBot/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types;
+
+namespace TGBot
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Host mamonBot = new Host("7509758606:AAEeefHNvn8STsXxGykjdNf63ghrEQYT0Gk");
+            mamonBot.BotStart();
+            mamonBot.OnMessage += OnMessage;
+            Console.ReadLine();
+        }
+
+        private static async void OnMessage(ITelegramBotClient client, Update update)
+        {
+            if (update.Message?.Text == "/start")
+            {
+                await client.SendTextMessageAsync(update.Message?.Chat.Id ?? 1992185162, "Darova!", replyToMessageId: update.Message?.MessageId);
+
+            }
+            else if (update.Message?.Text == "/about")
+            {
+                await client.SendTextMessageAsync(update.Message?.Chat.Id ?? 1992185162, "Це початкова структура для бота, тут ще нічого толкового не реалізовано.", replyToMessageId: update.Message?.MessageId);
+            }
+            else
+            {
+                await client.SendTextMessageAsync(update.Message?.Chat.Id ?? 1992185162, update.Message?.Text ?? "Ne text", replyToMessageId: update.Message?.MessageId);
+
+            }
+        }
+    }
+}
